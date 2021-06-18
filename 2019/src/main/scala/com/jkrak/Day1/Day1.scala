4ab29878ca38923a873be57738b95fc4534d69b7
Krak888 <jean.crnokrak@gmail.com>
2021-06-18 20:54:31 +0200

    Part II of day 1

--- 2019/src/main/scala/com/jkrak/Day1/Day1.scala
+++ 2019/src/main/scala/com/jkrak/Day1/Day1.scala
@@ -5,12 +5,19 @@ object Day1 {
     val inputRaw = ReadInput.readInputTextFile("day1","inputDay1")
     val inputClean = inputRaw.map(_.toInt)
 
-    println("sum is : " + sumNeededFuel(inputClean))
+    println("sum is : " + sumNeededFuelRecursive(inputClean))
   }
 
+  def fuelNeeded(mass: Int): Int = if (mass > 6) mass / 3 - 2 else 0
 
-  def fuelNeeded(mass: Int): Int = mass / 3 - 2
   def sumNeededFuel(massArray: Array[Int]): Int = massArray.map(fuelNeeded).sum
+
+
+  def sumNeededFuelRecursive(massArray: Array[Int]): Int = {
+    if (massArray.sum == 0) massArray.sum
+    else massArray.map(fuelNeeded).sum + sumNeededFuelRecursive(massArray.map(fuelNeeded))
+  }
+
 
 }
 
--- build.sbt
+++ build.sbt
@@ -1,9 +1,5 @@
 
 lazy val guava = "com.google.guava" % "guava" % "18.0"
-//lazy val sparkCore = "org.apache.spark" %% "spark-core" % "2.4.5"
-lazy val sparkSQL = "org.apache.spark" %% "spark-sql" % "2.4.5"
-
-lazy val commonDependencies = Seq(guava, sparkSQL)
 
 lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.3.0-SNAP2" % Test
 lazy val mockito = "org.mockito" %% "mockito-scala" % "1.14.4"
@@ -15,7 +11,6 @@ lazy val commonSettings = Seq(
   organization := "com.jKrak",
   version := "1.0",
   scalaVersion := "2.13.1",
-  libraryDependencies ++= commonDependencies,
   libraryDependencies ++= commonTestDependencies
 )
 
